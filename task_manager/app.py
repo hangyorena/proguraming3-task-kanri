d8419674803646e7e96ce5c14da38dbca0b868a7
yuge_32_rena <i23042@yuge.kosen-ac.jp>
2026-07-22 22:20:57 +0900

    Update app.py

--- task_manager/app.py
+++ task_manager/app.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template
+import json
 
+app = Flask(__name__)
+
+# -----------------------------
+# タスク一覧
+# -----------------------------
+@app.route("/")
+def task_list():
+
+    # JSONファイルを開く
+    with open("data/tasks.json", "r", encoding="utf-8") as file:
+        tasks = json.load(file)
+
+    # HTMLへ渡す
+    return render_template(
+        "task_list.html",
+        tasks=tasks
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
