84967f7039860047c9058e8fffed5da756f6c347
yuge_32_rena <i23042@yuge.kosen-ac.jp>
2026-07-22 23:14:30 +0900

    Update app.py

--- task_manager/app.py
+++ task_manager/app.py
@@ -148,6 +148,19 @@ def update_task(id):
 
     return redirect("/task")
 
+from flask import Flask
 
+from auth import auth_bp
+from task import task_bp
+from calendar import calendar_bp
+
+app = Flask(__name__)
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(task_bp)
+app.register_blueprint(calendar_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 if __name__ == "__main__":
     app.run(debug=True)
